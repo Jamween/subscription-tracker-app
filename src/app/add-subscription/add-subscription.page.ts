c9d0a796dd309326ec7c958f84860ffaef790826
Jamween <77840853+Jamween@users.noreply.github.com>
2025-04-27 15:27:11 +0100

    added subscription page with forms

--- src/app/add-subscription/add-subscription.page.ts
+++ src/app/add-subscription/add-subscription.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-subscription',
   templateUrl: './add-subscription.page.html',
   styleUrls: ['./add-subscription.page.scss'],
 })
-export class AddSubscriptionPage implements OnInit {
+export class AddSubscriptionPage {
+  subscription = {
+    name: '',
+    amount: null,
+    date: '',
+    billingCycle: 'monthly',
+    paymentMethod: ''
+  };
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  saveSubscription() {
+    console.log('Saving subscription:', this.subscription);
+    
+    this.router.navigate(['/home']);
   }
-
 }
